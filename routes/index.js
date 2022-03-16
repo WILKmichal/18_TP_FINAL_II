60d0acadfc13fef48b97e0e53068ff0c757159be
Tarik Daouine <tarikpro19@gmail.com>
2022-03-16 16:52:09 +0100

    Changement du CSS
    J'ai un peu changer les détails mais pas ouf zebi

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'truc à faire' });
-  
+  res.render('index', { title: 'truc à faire' }); 
 });
 
 module.exports = router;
-
-

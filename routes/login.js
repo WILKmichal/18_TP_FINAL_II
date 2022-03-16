b66f3a03ab52cd3c80a773fc017945a2bb54f41a
Tarik Daouine <tarikpro19@gmail.com>
2022-03-16 15:42:04 +0100

    traduction des formulaires

--- routes/login.js
+++ routes/login.js
@@ -1,32 +1,16 @@
-// const express = require("express");
-
-// const {
-//   registerView,
-//   loginView,
-//   registerUser,
-//   loginUser,
-// } = require("../controllers/loginController");
-// // const { dashboardView } = require("../controllers/dashboardController");
-// // const { protectRoute } = require("../auth/protect");
-
+// //js
+// const express = require('express');
+// const {registerView, loginView } = require('../controllers/loginController');
 // const router = express.Router();
-
-// router.get("/register", registerView);
-// router.get("/login", loginView);
-
-// //Dashboard
-// // router.get("/dashboard", protectRoute, dashboardView);loginUserregisterUser
-
-// router.post("/register");
-// router.post("/login");
-
+// router.get('/register', registerView);
+// router.get('/login', loginView);
 // module.exports = router;
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+  res.render('login', { title: 'Login' });
 });
 
 module.exports = router;

14493087dbb35c4a6f7f64269237cbab328375b3
Tarik Daouine <tarikpro19@gmail.com>
2022-03-16 14:24:10 +0100

    Affichage Login/register
    Le front est afficher

--- routes/register.js
+++ routes/register.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'truc à faire' });
+  res.render('register', { title: 'Express' });
 });
 
 module.exports = router;

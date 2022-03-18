7802f9d9a57880bee31609b8b82bdb6412e879b7
LAPTOP-8EC2G9HV\micwi <michal781998@protonmail.com>
2022-03-18 16:19:35 +0100

    recup contact from db for pdf export

--- controllers/pdfController.js
+++ controllers/pdfController.js
@@ -3,10 +3,11 @@ var router = express.Router();
 const Contact = require("../models/Contact");
 
 const contactPdf = async (req, res) => {
-    const contacts = await Contact.find(user_id = req.id);
-    contacts.forEach((contact) => {
-        console.log(contact.firstName, contact.lastName, contact.organization, contact.photo, contact.workPhone, contact.birthday, contact.title, contact.url)
-    })
+    const contact = await Contact.findById(req.query.id);
+    console.log(contact);
+    // contacts.forEach((contact) => {
+    //     console.log(contact.firstName, contact.lastName, contact.organization, contact.photo, contact.workPhone, contact.birthday, contact.title, contact.url)
+    // })
 
 
 }

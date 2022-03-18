08034edb7d9bf90b8ea371942fbcd6b4088ecec2
Ronan Royet <royet_r@etna-alternance.net>
2022-03-18 14:46:50 +0100

    add QR

--- models/Contact.js
+++ models/Contact.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const contactSchema = new mongoose.Schema({
     firstName: {
         type: String,
-        required: false,
+        required: true,
     },
     lastName: {
         type: String,

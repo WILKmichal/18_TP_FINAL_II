1203f7f1508f4be25cf312535b884ed6a60e6500
Ronan Royet <royet_r@etna-alternance.net>
2022-03-18 14:46:50 +0100

    add QR

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -12,27 +12,80 @@ const contactListRedirect = async (req, res) => {
     res.redirect('/contact/list');
 }
 
+function fString(property) {
+    if (!property) return "";
+
+    return property + ";"
+}
+
+function stringIfSet(property) {
+    if (!property) return "";
+    return "" + property;
+}
+
+function buildVCardQRString(contact) {
+    const vcard = {
+        str_vcard: 'BEGIN:VCARD\nVERSION:3.0\n',
+        str_end: '\nEND:VCARD',
+        goog_chart: 'https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl='
+    }
+    let N = "";
+    N += fString(contact.title);
+    N += fString(contact.lastName);
+    N += fString(contact.firstName);
+
+    let FN = "";
+    FN += stringIfSet(contact.title) + " ";
+    FN += stringIfSet(contact.lastName) + " ";
+    FN += stringIfSet(contact.firstName);
+
+    const ORG = fString(contact.organization);
+
+    const TEL = fString(contact.workPhone);
+
+    const BDAY = fString(contact.birthday);
+
+    const TITLE = fString(contact.title);
+
+    const URL = fString(contact.url);
+
+    vcard.str_vcard += `N:${N}\n`;
+    vcard.str_vcard += `FN:${FN}\n`;
+    vcard.str_vcard += `ORG:${ORG}\n`;
+    vcard.str_vcard += `TEL:${TEL}\n`;
+    vcard.str_vcard += `BDAY:${BDAY}\n`;
+    vcard.str_vcard += `TITLE:${TITLE}\n`;
+    vcard.str_vcard += `URL:${URL}\n`;
+
+
+
+    vcard.str_vcard += vcard.str_end;
+
+    return vcard.goog_chart + encodeURIComponent(vcard.str_vcard);
+
+}
+
 const contactShowView = async (req, res) => {
     const id = req.params.id;
     const contact = await Contact.findById(id).exec();
-    console.log(contact.user_id)
-    console.log(req.user.id)
     if (!(contact.user_id == req.user.id)) {
         console.log("redirect")
         res.redirect('/contact/list');
         return;
     }
-    
-    console.log('render')
+
+    const QR = buildVCardQRString(contact);
+
     res.render("contact-show", {
-        contact: contact
+        contact: contact,
+        qr: QR
     });
 }
 
 const contactUpdateView = async (req, res) => {
     const id = req.params.id;
     const contact = await Contact.findById(id).exec();
-    if (!contact.user_id == req.user.id) {
+    if (!(contact.user_id == req.user.id)) {
         res.redirect('/contact/list');
         return;
     }
